7cacfe07004a03ac6f1ce539d1883b793a35433e
uzzybotak <knightindustriesfans@yahoo.com>
2014-05-17 13:39:30 +0700

    Script Maju

---
+++ Pay2Win/Assets/Scripts/Necessary/Maju.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	
+	public float speed = -1.0f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow))
+			transform.Translate (speed * Vector2.right * Time.deltaTime);
+		else if(Input.GetKey(KeyCode.LeftArrow))
+			transform.Translate (-speed * Vector2.right * Time.deltaTime);
+	}
+}
